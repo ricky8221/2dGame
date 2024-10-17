53adb1da91be14a2b642c6a7bfce8360b5e24bce
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2024-10-16 22:49:12 -0700

    create canvas, rec represent character, movement keyboard listener, and fps tracker

--- src/main/java/com/example/my2dgame/Main.java
+++ src/main/java/com/example/my2dgame/Main.java
@@ -9,7 +9,14 @@ public class Main {
         window.setResizable(false);
         window.setTitle("2D Adventure");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }
